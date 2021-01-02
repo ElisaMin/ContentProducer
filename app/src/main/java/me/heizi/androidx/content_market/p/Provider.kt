c9ea20186cd30c028710c08c2ca37a6042b19d97
Heizi <xm.zhongjiaxin@gmail.com>
2021-01-02 18:24:15 +0800

    删除冗余

--- app/src/main/java/me/heizi/androidx/content_market/p/Provider.kt
+++ app/src/main/java/me/heizi/androidx/content_market/p/Provider.kt
@@ -1,47 +1,45 @@
 package me.heizi.androidx.content_market.p
-
-import android.content.ContentValues
-import android.net.Uri
-import me.heizi.androidx.content_market.p.ContentProducer
-import me.heizi.androidx.content_market.p.Controllers
-import me.heizi.androidx.content_market.p.controller.InsertController
-import me.heizi.androidx.content_market.p.controller.QueryController
-import me.heizi.androidx.content_market.p.controller.interfaces.ForDelete
-import me.heizi.androidx.content_market.p.dao.Dao
-import me.heizi.androidx.content_market.p.dao.Databases
-
-class  Provider  : ContentProducer<InsertController, QueryController, ForDelete, Any?>(
-        "examble.test"
-)  {
-    override val dao: Dao
-        get() = Databases.Dao
-
-
-    override fun getType(uri: Uri): String {
-        return "vnd.android.cursor.dir/abc"
-    }
-
-    override fun onCreate(): Boolean {
-        Databases.getInstance(context!!)
-        return true
-    }
-
-    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-        TODO("Implement this to handle requests to delete one or more rows")
-    }
-    override fun update(
-        uri: Uri, values: ContentValues?, selection: String?,
-        selectionArgs: Array<String>?
-    ): Int {
-        TODO("Implement this to handle requests to update one or more rows.")
-    }
-
-    override val controllers = object: Controllers<InsertController, QueryController, ForDelete, Any?> {
-                override val insert: InsertController = InsertController(baseUri)
-                override val query: QueryController = QueryController(dao,baseUri)
-                override val delete: ForDelete = ForDelete(baseUri,dao)
-                override val update = null
-    }
-
-
-}
+// EXAM
+//import android.content.ContentValues
+//import android.net.Uri
+//import me.heizi.androidx.content_market.p.controller.InsertController
+//import me.heizi.androidx.content_market.p.controller.QueryController
+//import me.heizi.androidx.content_market.p.controller.interfaces.ForDelete
+//import me.heizi.androidx.content_market.p.dao.Dao
+//
+//
+//class  Provider  : ContentProducer<InsertController, QueryController, ForDelete, Any?>(
+//        "examble.test"
+//)  {
+//    override val dao: Dao
+//        get() = throw ""
+//
+//
+//    override fun getType(uri: Uri): String {
+//        return "vnd.android.cursor.dir/abc"
+//    }
+//
+//    override fun onCreate(): Boolean {
+//        Databases.getInstance(context!!)
+//        return true
+//    }
+//
+//    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+//        TODO("Implement this to handle requests to delete one or more rows")
+//    }
+//    override fun update(
+//        uri: Uri, values: ContentValues?, selection: String?,
+//        selectionArgs: Array<String>?
+//    ): Int {
+//        TODO("Implement this to handle requests to update one or more rows.")
+//    }
+//
+//    override val controllers = object: Controllers<InsertController, QueryController, ForDelete, Any?> {
+//                override val insert: InsertController = InsertController(baseUri)
+//                override val query: QueryController = QueryController(dao,baseUri)
+//                override val delete: ForDelete = ForDelete(baseUri,dao)
+//                override val update = null
+//    }
+//
+//
+//}
--- app/src/main/java/me/heizi/androidx/content_market/p/controller/InsertController.kt
+++ app/src/main/java/me/heizi/androidx/content_market/p/controller/InsertController.kt
@@ -1,11 +1,8 @@
 package me.heizi.androidx.content_market.p.controller
 
-import android.content.ContentValues
-import android.net.Uri
 import me.heizi.androidx.content_market.annotations.Path
 import me.heizi.androidx.content_market.p.controller.interfaces.ForInsert
 import me.heizi.androidx.content_market.p.teamplates.InsertTemplate
-import me.heizi.androidx.content_market.p.utils.convert
 
 
 @Path("/put")
@@ -15,14 +12,14 @@ class InsertController constructor (
 
     private val template = InsertTemplate(baseUri)
 
-    @Path("/types")
-    fun addType(values: ContentValues): Uri = template.runCatching {
-        me.heizi.androidx.content_market.p.dao.Databases.dao.addType(values.convert())
-    }
-
-    @Path("/publisher")
-    fun addPublisher(values: ContentValues) = template.runCatching {
-        me.heizi.androidx.content_market.p.dao.Databases.dao.addPublisher(values.convert())
-    }
+//    @Path("/types")
+//    fun addType(values: ContentValues): Uri = template.runCatching {
+//        me.heizi.androidx.content_market.p.dao.Databases.dao.addType(values.convert())
+//    }
+//
+//    @Path("/publisher")
+//    fun addPublisher(values: ContentValues) = template.runCatching {
+//        me.heizi.androidx.content_market.p.dao.Databases.dao.addPublisher(values.convert())
+//    }
 
 }
